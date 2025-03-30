cadd6548999f8d8d9f02a182968294276e49fc9b
benamiller <mrbenamiller@gmail.com>
2025-03-30 21:45:11 +0100

    Add types and tests for their expected existence

---
+++ src/types.zig
@@ -0,0 +1,3 @@
+pub const DType = enum { f32, i32, bool };
+pub const Device = enum { CPU, GPU };
+
---
+++ test/core/types_test.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+const types = @import("../../src/types.zig");
+
+test "expect DType.f32 existence" {
+    const tag_name = @tagName(types.DType.f32);
+    try std.testing.expectEqualStrings("f32", tag_name);
+}
+
+test "expect DType.i32 existence" {
+    const tag_name = @tagName(types.DType.i32);
+    try std.testing.expectEqualStrings("i32", tag_name);
+}
+
+test "expect DType.bool existence" {
+    const tag_name = @tagName(types.DType.bool);
+    try std.testing.expectEqualStrings("bool", tag_name);
+}
+
+test "expect CPU device" {
+    const tag_name = @tagName(types.DType.CPU);
+    try std.testing.expectEqualStrings("CPU", tag_name);
+}
+
+test "expect GPU device" {
+    const tag_name = @tagName(types.DType.GPU);
+    try std.testing.expectEqualStrings("GPU", tag_name);
+}
