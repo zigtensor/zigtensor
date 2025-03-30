11a1bfcd72f9b3fdc68f34af8b4a75c6de85a98a
benamiller <mrbenamiller@gmail.com>
2025-03-30 17:12:08 +0100

    liblib.a is sus

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
-        .name = "lib",
+        .name = "zigtensor",
         .root_source_file = b.path("./src/lib.zig"),
         .target = target,
         .optimize = optimize,
