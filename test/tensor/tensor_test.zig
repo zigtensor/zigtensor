745b3d64d3ec3cf672359c30220f68edf829489e
benamiller <mrbenamiller@gmail.com>
2025-03-31 13:52:42 +0100

    Establish convention of instantiation within blocks

--- test/tensor/tensor_test.zig
+++ test/tensor/tensor_test.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 const tensor = @import("../../src/tensor/tensor.zig");
 const types = @import("../../src/types.zig");
 
-const t = tensor.Tensor{
-    .shape = &[_]usize{2,3},
-    .strides = &[_]usize{3,1},
-    .dtype = types.DType.f32,
-    .device_id = types.Device.CPU,
-};
-
 test "expect tensor shape to be {2,3}" {
+    const t = tensor.Tensor{
+        .shape = &[_]usize{2,3},
+        .strides = &[_]usize{3,1},
+        .dtype = types.DType.f32,
+        .device_id = types.Device.CPU,
+    };
+
     try std.testing.expect(t.shape.len == 2);
 }
