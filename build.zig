e092a42e4466fd3b72df55e9eaed4cfe9ebf9c82
Benjamin Miller <mrbenamiller@gmail.com>
2026-01-22 22:33:20 +0000

    Revival begins... Still broken

--- build.zig
+++ build.zig
@@ -10,20 +10,19 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zigtensor",
+    const lib = b.addLibrary(.{ .name = "zigtensor", .linkage = .static, .root_module = b.createModule(.{
         .root_source_file = b.path("./src/lib.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
     b.installArtifact(lib);
 
     const test_step = b.step("test", "Run unit tests");
-    const unit_tests = b.addTest(.{
+    const unit_tests = b.addTest(.{ .root_module = b.createModule(.{
         .root_source_file = b.path("test/root.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
 
     unit_tests.root_module.addImport("zigtensor", zigtensor_module);
 
